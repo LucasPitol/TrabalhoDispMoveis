6c3a0842160ed11da79584a2ac2c950b8d500877
Pitol <lucascpitol0@gmail.com>
2019-10-02 22:01:43 -0300

    Sensor de movimentos

--- app/src/main/java/com/example/mexemexe/MainActivity.java
+++ app/src/main/java/com/example/mexemexe/MainActivity.java
@@ -4,13 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     private ImageView startBtn;
     private ImageView stopBtn;
     private ImageView cleanBtn;
     private ImageView gambiSpace;
+    private TextView texto;
+
+    private TextView xPosition;
+    private TextView yPosition;
+    private TextView zPosition;
+
+    private Sensor sensor;
+    private SensorManager gerenciadorMovimento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setUI();
+
+        startSensor();
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,14 +51,38 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void startSensor() {
+        gerenciadorMovimento = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        sensor = gerenciadorMovimento.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        gerenciadorMovimento.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
     public void setUI() {
         startBtn = findViewById(R.id.start_btn);
         stopBtn = findViewById(R.id.stop_btn);
         cleanBtn = findViewById(R.id.clean_icon);
         gambiSpace = findViewById(R.id.gambi);
+        texto = findViewById(R.id.texto);
+
+        xPosition = findViewById(R.id.xPosition);
+        yPosition = findViewById(R.id.yPosition);
+        zPosition = findViewById(R.id.zPosition);
 
         stopBtn.setVisibility(View.GONE);
         cleanBtn.setVisibility(View.GONE);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xPosition.setText("X: " + event.values[0]);
+        yPosition.setText("Y: " + event.values[1]);
+        zPosition.setText("Z: " + event.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //Não utilizar
+    }
 }
